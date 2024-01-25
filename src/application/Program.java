1b91774296b435837be1ac01685bc10144bdca14
edsonwisses <wisses@gmail.com>
2024-01-25 18:39:06 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert ====");
+		/*System.out.println("\n=== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greb@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("Inserted! New id = " + newSeller.getId());*/
+		
+		System.out.println("\n=== TEST 5: seller insert ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 
 	}
 
